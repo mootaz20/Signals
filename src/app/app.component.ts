a0589477f8c01ee23bd4a25e0f0563fd62c17559
mootaz20 <mootazalhalak695@gmail.com>
2024-03-25 00:34:39 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test2';
+  counter = signal(0);
+  msg = signal<string[]>([]);
+  division = computed(()=> this.counter() / 2);
+  double = computed(()=> this.counter() * 2);
+
+  increment(){
+    this.counter.update(c =>  c + 1);
+    this.msg.mutate(m => m.push('The counter is : ' + this.counter()));
+  }
+  decrement(){
+    this.counter.update(c =>  c - 1);
+    this.msg.mutate(m => m.pop());
+  }
+  reset(){
+    this.counter.set(0);
+  }
+  doubleCounter(){
+    this.msg.mutate(m => m.push('The counter is : ' + this.double()));
+    this.counter.set(this.double());
+  }
+
+
 }
